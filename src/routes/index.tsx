8d40f4fa68f23c34d74fa2b5336dcbd23cfa018b
Meen223 <meenboyd223@gmail.com>
2025-05-16 22:47:34 +0700

    อัปเดตโค้ดเวอร์ชันล่าสุดของฉัน

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,23 +2,23 @@ import { type RouteObject, Navigate } from "react-router-dom";
 import ActivityList from "../pages/ActivityList";
 import ActivityDetail from "../pages/ActivityDetail";
 import LoginPage from "../pages/LoginPage";
-import NewActivity from "../pages/NewActivity";
-import EditActivityDetail from "../pages/EditActivityDetail";
-import EditActivities from "../pages/EditActivitiesList";
-import ApproveActivity from "../pages/ApproveActivity";
-import CancelActivity from "../pages/CancelActivity";
+// import NewActivity from "../pages/NewActivity";
+// import EditActivityDetail from "../pages/EditActivityDetail";
+// import EditActivities from "../pages/EditActivitiesList";
+// import ApproveActivity from "../pages/ApproveActivity";
+// import CancelActivity from "../pages/CancelActivity";
 
 export const routes: RouteObject[] = [
   
   { path: "/activities", element: <ActivityList /> },
   { path: "/activities/:id", element: <ActivityDetail /> },
-  { path: "/activities/new", element: <NewActivity /> },
+  // { path: "/activities/new", element: <NewActivity /> },
   { path: "/login", element: <LoginPage /> },
   { path: "/activities/:id", element: <ActivityDetail /> },
-  { path: "/activities/edit", element: <EditActivities /> },
-  { path: "/activities/:id/edit",element: <EditActivityDetail />,},
-  { path: "/activities/:id/approve", element: <ApproveActivity /> },
-  { path: "/activities/:id/cancel", element: <CancelActivity /> },
+  // { path: "/activities/edit", element: <EditActivities /> },
+  // { path: "/activities/:id/edit",element: <EditActivityDetail />,},
+  // { path: "/activities/:id/approve", element: <ApproveActivity /> },
+  // { path: "/activities/:id/cancel", element: <CancelActivity /> },
   { path: "/home", element: <Navigate to="/" replace /> },
 
 ];
